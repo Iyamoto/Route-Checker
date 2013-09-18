ec6d31af88bfadbb44deddaaa28fc98fc65dc750
Iyamoto <Iyamoto@users.noreply.github.com>
2013-09-18 16:12:01 +0400

    sh

--- sample-checker.sh
+++ sample-checker.sh
@@ -2,10 +2,8 @@
 #rename sample-checker.sh to checker.sh
 #chmod +x checker.sh
 #replace basedir with your path to project
-#and add to /etc/crontab: */10 * * * * root /path/to/checker.sh >/dev/null 2>&1
+#and add to /etc/crontab: */15 * * * * root /path/to/checker.sh >/dev/null 2>&1
 basedir="/root/project"
 cd $basedir
 date > lastrun.log
 /usr/bin/php checker.php >> lastrun.log
-/usr/bin/php filter.php >> lastrun.log
-/usr/bin/php reporter.php >> lastrun.log
