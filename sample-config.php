5a4378d76ee134146e0db3f97edd6a46eac38ebe
Iyamoto <Iyamoto@users.noreply.github.com>
2013-09-19 06:58:57 +0400

    improve

--- sample-config.php
+++ sample-config.php
@@ -5,12 +5,10 @@
  * Rename to config.php
  */
 require_once 'functions.php';
-//$emails and $netflow_base_dir must be set 
-$emails[] = 'name@domain.zone'; //Whom to report
-$web_dir = '/var/www/routechecker';
+//INIT 
+$emails[] = 'root@localhost'; //Whom to report
 $db_dir = '/tmp/routechecker';
-$tpl_dir = 'tpl';
-$traceroute = '/usr/sbin/traceroute';//which traceroute
+$traceroute = '/usr/sbin/traceroute'; //which traceroute
 
 $today = date("Y-m-d");
 if (!is_dir($db_dir))
@@ -23,7 +21,7 @@ $targets['server1'] = '10.10.10.10';
 $targets['router1'] = '10.10.10.1';
 
 $debug = false;
+$skip_first_host = true;
 
 $test_results = '';
-$test_results2 = '';
 ?>
